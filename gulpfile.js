946a550ffe7b5887624794ed4561b96127f1875d
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-07-03 17:05:09 -0300

    Configure gulp (WIP)

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
+
+
+// File's paths
+var paths = {
+  mainSCSS: 'src/sass/main.scss',
+}
+
+
+// Compile SASS files.
+// Docs: https://github.com/dlmanning/gulp-sass#basic-usage
+gulp.task('sass', function() {
+  return gulp.src(paths.mainSCSS)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/sass'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./src/sass'))
+});
+
+
+// Rerun lint and jest tasks after changes on JavaScript files
+// Docs: https://github.com/gulpjs/gulp/blob/master/docs/API.md#gulpwatchglob-opts-cb
+gulp.task('watch', ['sass', 'webserver'], function() {
+  gulp.watch(paths.mainSCSS, ['sass']);
+});
+
+
+// Serve application locally
+// Docs: https://github.com/schickling/gulp-webserver#usage
+gulp.task('webserver', function() {
+  gulp.src('./src')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+    }));
+});
