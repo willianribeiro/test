9b4f55eebb4746828c015d213c153d14894d9447
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-07-04 16:30:00 -0300

    Add eslint task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var webserver = require('gulp-webserver');
+var eslint = require('gulp-eslint');
 
 
 // File's paths
 var paths = {
   mainSCSS: 'src/sass/main.scss',
-  allSCSS: 'src/**/*.scss'
+  allSCSS: 'src/**/*.scss',
+  allJavaScript: 'src/**/*.js',
 }
 
 
@@ -25,6 +27,16 @@ gulp.task('sass', function() {
 });
 
 
+// Lint JavaScript files.
+// docs: https://github.com/adametry/gulp-eslint#usage
+gulp.task('eslint', function() {
+  return gulp.src(paths.allJavaScript)
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+
 // Run sass and webserver tasks and watch for changes in .scss files.
 // Docs: https://github.com/gulpjs/gulp/blob/master/docs/API.md#gulpwatchglob-opts-cb
 gulp.task('watch', ['sass', 'webserver'], function() {
@@ -35,7 +47,7 @@ gulp.task('watch', ['sass', 'webserver'], function() {
 // Serve application locally
 // Docs: https://github.com/schickling/gulp-webserver#usage
 gulp.task('webserver', function() {
-  gulp.src('.')
+  return gulp.src('.')
     .pipe(webserver({
       livereload: true,
       open: true,
